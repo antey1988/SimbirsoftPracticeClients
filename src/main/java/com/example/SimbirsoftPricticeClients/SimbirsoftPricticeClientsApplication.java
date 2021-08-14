1ef31411e52a74c2f384ab2772ad1be2c421cc4b
Antey1988 <Antey1988@gmail.com>
2021-08-14 18:00:30 +0300

    init project

---
+++ src/main/java/com/example/SimbirsoftPricticeClients/SimbirsoftPricticeClientsApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimbirsoftPricticeClients;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimbirsoftPricticeClientsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimbirsoftPricticeClientsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimbirsoftPricticeClients/SimbirsoftPricticeClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimbirsoftPricticeClients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimbirsoftPricticeClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
